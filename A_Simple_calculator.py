830fd2cfa866302287e17ac5e0f966424d465451
Rana Vishal Singh <vishalrana.kaps19@gmail.com>
2019-02-23 08:15:37 +0530

    A_Simple_Calculator working with integers

---
+++ A_Simple_calculator.py
@@ -0,0 +1,24 @@
+#A _Simple_Calculator_which works with integer values
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+ch = input(""" Enter the operation you want to perform
+               + for Addition
+               - for Subtraction
+               * for Multiplication
+               / for Division
+               % for Modulus(to find reminder):-""")
+
+if ch is '+':
+    print("The sum of the given numbers is: ", num1 + num2)
+elif ch is '-':
+    print("The difference between the given numbers is: ", num1 - num2)
+elif ch is '*':
+    print("The product of the given numbers is: ", num1 * num2)
+elif ch is '/':
+    print("The quotient(Division) of the numbers is: ", num1 / num2)
+elif ch is '%':
+    print("The modulo(reminder) after the operation is: ", num1 % num2)
+else:
+    print("Please enter the correct operation")
